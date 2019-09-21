42ce161933d3aaf184caa2eccc16c99c2de942e2
wufei <331334402@qq.com>
2019-09-21 16:42:55 +0800

    登录功能

---
+++ src/components/Login.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="login">
+      <el-form ref="form" :model="form" :rules="rules" label-width="80px">
+        <img class="avatar" src="../assets/avatar.jpg" alt="">
+        <el-form-item label="用户名" prop="username">
+          <el-input placeholder="请输入用户名" v-model="form.username"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input placeholder="请输入密码" type="password" v-model="form.password"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button @click="login" class="login-btn" type="primary">登录</el-button>
+          <el-button @click="reset" plain>重置</el-button>
+        </el-form-item>
+      </el-form>
+  </div>
+</template>
+
+<script>
+import Axios from 'axios'
+export default {
+  data () {
+    return {
+      form: {
+        username: '',
+        password: ''
+      },
+      rules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: ['blur', 'change'] },
+          { min: 3, max: 12, message: '用户名长度在3到12位字符', trigger: ['blur', 'change'] }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: ['blur', 'change'] },
+          { min: 3, max: 12, message: '密码长度在3到12位字符', trigger: ['blur', 'change'] }
+        ]
+      }
+    }
+  },
+  methods: {
+    reset () {
+      this.$refs.form.resetFields()
+    },
+    login () {
+      this.$refs.form.validate(state => {
+        if (state) {
+          Axios.post('http://localhost:8888/api/private/v1/login', this.form).then(res => {
+            const { meta, data } = res.data
+            if (meta.status === 200) {
+              localStorage.setItem('token', data.token)
+              this.$message({
+                type: 'success',
+                message: '登陆成功!',
+                duration: 2000
+              })
+              this.$router.push({ name: 'index' })
+            } else {
+              this.$message({
+                type: 'error',
+                message: meta.msg,
+                duration: 2000
+              })
+            }
+          })
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  .login{
+    width:100%;
+    height:100%;
+    background-color:#2D434C;
+    overflow:hidden;
+    .el-form {
+      width:400px;
+      padding:20px;
+      padding-top:70px;
+      background-color:white;
+      margin:300px auto;
+      position: relative;
+      .avatar {
+        position:absolute;
+        left:50%;
+        top:-50px;
+        border-radius: 50%;
+        border:5px solid white;
+        transform:translateX(-50%)
+      }
+      .login-btn {
+        margin-right:80px;
+      }
+    }
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import router from './router'
 
+Vue.use(ElementUI)
+
 Vue.config.productionTip = false
+
+router.beforeEach((to, from, next) => {
+  const token = localStorage.getItem('token')
+  if (token || to.path === '/login') {
+    next()
+  } else {
+    next({ name: 'login' })
+  }
+})
 
 new Vue({
   router,
